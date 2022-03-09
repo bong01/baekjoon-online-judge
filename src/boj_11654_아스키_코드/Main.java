28ebdfee0ab234671fd2e75420a7c151033e40bd
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-03-09 18:29:11 +0900

    아스키 코드

---
+++ src/boj_11654_아스키_코드/Main.java
@@ -0,0 +1,12 @@
+package boj_11654_아스키_코드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.read());
+    }
+}
