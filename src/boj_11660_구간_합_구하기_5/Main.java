14fa4384756c35b2166fa08d8a82422c5271919f
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-11-25 16:34:39 +0900

    [백준] 구간 합 구하기 5

---
+++ src/boj_11660_구간_합_구하기_5/Main.java
@@ -0,0 +1,39 @@
+package boj_11660_구간_합_구하기_5;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n + 1][n + 1];
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + arr[i][j] - dp[i - 1][j - 1];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            int result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+            sb.append(result).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
